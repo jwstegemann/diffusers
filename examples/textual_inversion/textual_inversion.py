2db8fd6953ef26cf9cce0444899d10a2d0fa16b7
Jens Stegemann <jwstegemann@gmail.com>
2023-10-05 18:12:30 +0200

    skjdf

--- examples/textual_inversion/textual_inversion.py
+++ examples/textual_inversion/textual_inversion.py
@@ -446,29 +446,29 @@ def parse_args():
 
 
 imagenet_templates_small = [
-    "a {}, {}"
+    ""
 ]
 
 imagenet_style_templates_small = [
-    "a painting in the style of {}, {}",
-    "a rendering in the style of {}, {}",
-    "a cropped painting in the style of {}, {}",
-    "the painting in the style of {}, {}",
-    "a clean painting in the style of {}, {}",
-    "a dirty painting in the style of {}, {}",
-    "a dark painting in the style of {}, {}",
-    "a picture in the style of {}, {}",
-    "a cool painting in the style of {}, {}",
-    "a close-up painting in the style of {}, {}",
-    "a bright painting in the style of {}, {}",
-    "a cropped painting in the style of {}, {}",
-    "a good painting in the style of {}, {}",
-    "a close-up painting in the style of {}, {}",
-    "a rendition in the style of {}, {}",
-    "a nice painting in the style of {}, {}",
-    "a small painting in the style of {}, {}",
-    "a weird painting in the style of {}, {}",
-    "a large painting in the style of {}, {}",
+    "a painting in the style of ",
+    "a rendering in the style of ",
+    "a cropped painting in the style of ",
+    "the painting in the style of ",
+    "a clean painting in the style of ",
+    "a dirty painting in the style of ",
+    "a dark painting in the style of ",
+    "a picture in the style of ",
+    "a cool painting in the style of ",
+    "a close-up painting in the style of ",
+    "a bright painting in the style of ",
+    "a cropped painting in the style of ",
+    "a good painting in the style of ",
+    "a close-up painting in the style of ",
+    "a rendition in the style of ",
+    "a nice painting in the style of ",
+    "a small painting in the style of ",
+    "a weird painting in the style of ",
+    "a large painting in the style of ",
 ]
 
 
@@ -531,7 +531,8 @@ class TextualInversionDataset(Dataset):
         #bcddddddd
         filewords = self.captions[i % self.num_images]
         #print("filewords", filewords)
-        text = random.choice(self.templates).format(placeholder_string, filewords)
+        #text = random.choice(self.templates).format(placeholder_string, filewords)
+        text = f"{random.choice(self.templates)} {filewords}".format(placeholder_string)
         #print(f"get {text} from dataset for {image_path}")
 
         example["input_ids"] = self.tokenizer(
