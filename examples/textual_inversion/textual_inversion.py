d064c27875e7ca1c9c493bc00a4cd3eae3503fda
Jens Stegemann <jwstegemann@gmail.com>
2023-10-05 16:44:33 +0200

    e

--- examples/textual_inversion/textual_inversion.py
+++ examples/textual_inversion/textual_inversion.py
@@ -446,29 +446,29 @@ def parse_args():
 
 
 imagenet_templates_small = [
-    "a {} {}"
+    "a {}, {}"
 ]
 
 imagenet_style_templates_small = [
-    "a painting in the style of {} {}",
-    "a rendering in the style of {} {}",
-    "a cropped painting in the style of {} {}",
-    "the painting in the style of {} {}",
-    "a clean painting in the style of {} {}",
-    "a dirty painting in the style of {} {}",
-    "a dark painting in the style of {} {}",
-    "a picture in the style of {} {}",
-    "a cool painting in the style of {} {}",
-    "a close-up painting in the style of {} {}",
-    "a bright painting in the style of {} {}",
-    "a cropped painting in the style of {} {}",
-    "a good painting in the style of {} {}",
-    "a close-up painting in the style of {} {}",
-    "a rendition in the style of {} {}",
-    "a nice painting in the style of {} {}",
-    "a small painting in the style of {} {}",
-    "a weird painting in the style of {} {}",
-    "a large painting in the style of {} {}",
+    "a painting in the style of {}, {}",
+    "a rendering in the style of {}, {}",
+    "a cropped painting in the style of {}, {}",
+    "the painting in the style of {}, {}",
+    "a clean painting in the style of {}, {}",
+    "a dirty painting in the style of {}, {}",
+    "a dark painting in the style of {}, {}",
+    "a picture in the style of {}, {}",
+    "a cool painting in the style of {}, {}",
+    "a close-up painting in the style of {}, {}",
+    "a bright painting in the style of {}, {}",
+    "a cropped painting in the style of {}, {}",
+    "a good painting in the style of {}, {}",
+    "a close-up painting in the style of {}, {}",
+    "a rendition in the style of {}, {}",
+    "a nice painting in the style of {}, {}",
+    "a small painting in the style of {}, {}",
+    "a weird painting in the style of {}, {}",
+    "a large painting in the style of {}, {}",
 ]
 
 
